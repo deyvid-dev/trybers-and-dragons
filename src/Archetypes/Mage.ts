3d673962c045f5ddc303ef9da578dba2097e8618
deyvid-dev <deyvidscm@hotmail.com>
2022-08-14 19:45:26 -0300

    Req-05 - Crie classes que herdam de Archetype

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,24 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static _createdArchetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    this._energyType = 'mana';
+    Mage._createdArchetypeInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage._createdArchetypeInstances;
+  }
+}
+
+export default Mage;
