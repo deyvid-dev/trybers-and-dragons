2b6e25b2ec31514e3c5286b52e84b49f378de70a
deyvid-dev <deyvidscm@hotmail.com>
2022-08-14 15:42:19 -0300

    Req02 - Crie classes que herdam de Race

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 80;
+    Dwarf._createdRacesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._createdRacesInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Dwarf;
---
+++ src/Races/Elf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 99;
+    Elf._createdRacesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf._createdRacesInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Elf;
